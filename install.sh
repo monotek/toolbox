ef243e2039128497b53ce715ff70f8861dd510d8
André Bauer <monotek@users.noreply.github.com>
2025-11-24 12:12:24 +0100

    Modify install.sh to streamline package installations
    Updated install script to include helm, kind, kubectl, and yq-go while removing their individual installation steps.

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env sh
+ #!/usr/bin/env sh
 
 set -ex
 
 # install apk packages
 apk update 
 apk --no-cache upgrade
-apk --no-cache add ca-certificates curl libcap openssh openssl jq rsync util-linux-misc
+apk --no-cache add ca-certificates curl helm kind kubectl kustomize libcap openssh openssl jq rsync util-linux-misc yq-go
 
 # add toolbox user
 adduser -S toolbox toolbox
@@ -21,31 +21,10 @@ curl --silent --show-error --fail --location --output /tmp/cue.tar.gz https://gi
 tar -C /usr/local/bin -xf /tmp/cue.tar.gz cue
 rm /tmp/cue.tar.gz
 
-# install helm
-curl --silent --show-error --fail --location --output /tmp/helm.tar.gz https://get.helm.sh/helm-"${HELM_VERSION}"-linux-"${ARCH}".tar.gz
-mkdir /tmp/helm
-tar -C /tmp/helm -xf /tmp/helm.tar.gz
-cp /tmp/helm/linux-"${ARCH}"/helm /usr/local/bin/helm
-rm -rf /tmp/helm.tar.gz /tmp/helm
-
-# install kind
-curl --silent --show-error --fail --location --output /usr/local/bin/kind https://kind.sigs.k8s.io/dl/"${KIND_VERSION}"/kind-linux-"${ARCH}"
-chmod 755 /usr/local/bin/kind
-
 # install kubeconform
 curl --silent --show-error --fail --location --output /tmp/kubeconform.tar.gz https://github.com/yannh/kubeconform/releases/download/"${KUBECONFORM_VERSION}"/kubeconform-linux-"${ARCH}".tar.gz
 tar -C /usr/local/bin -xf /tmp/kubeconform.tar.gz kubeconform
 rm /tmp/kubeconform.tar.gz
-
-# install kubectl
-curl --silent --show-error --fail --location --output /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/"${KUBECTL_VERSION}"/bin/linux/"${ARCH}"/kubectl
-chmod 755 /usr/local/bin/kubectl
-
-# install kustomize
-curl --silent --show-error --fail --location --output /tmp/kustomize.tar.gz https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F"${KUSTOMIZE_VERSION}"/kustomize_"${KUSTOMIZE_VERSION}"_linux_"${ARCH}".tar.gz
-tar -C /usr/local/bin -xf /tmp/kustomize.tar.gz kustomize
-#chmod 755 /usr/local/bin/kustomize
-rm /tmp/kustomize.tar.gz
 
 # install sops
 curl --silent --show-error --fail --location --output /usr/local/bin/sops https://github.com/getsops/sops/releases/download/"${SOPS_VERSION}"/sops-"${SOPS_VERSION}".linux."${ARCH}"
@@ -55,10 +34,6 @@ chmod 755 /usr/local/bin/sops
 curl --silent --show-error --fail --location --output /tmp/terraform.zip https://releases.hashicorp.com/terraform/"${TERRAFORM_VERSION}"/terraform_"${TERRAFORM_VERSION}"_linux_"${ARCH}".zip
 unzip /tmp/terraform.zip -d /usr/local/bin
 rm /tmp/terraform.zip
-
-# install yq
-curl --silent --show-error --fail --location --output /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/"${YQ_BIN_VERSION}"/yq_linux_"${ARCH}"
-chmod 755 /usr/local/bin/yq
 
 # set permissions
 mkdir -p /data
